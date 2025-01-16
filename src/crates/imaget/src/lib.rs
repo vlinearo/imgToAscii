167c0b51d03c774cd92908245a7250b3e05a87cd
vlinearo <vlinearo@gmail.com>
2025-01-16 22:42:32 +0200

    fix some bugs; complete convertor

--- src/crates/imaget/src/lib.rs
+++ src/crates/imaget/src/lib.rs
@@ -10,8 +10,6 @@ use std::path::PathBuf;
 use image::{
     DynamicImage,
     GenericImageView,
-    ImageBuffer,
-    Luma
 };
 
 use tokio::task;
@@ -25,7 +23,8 @@ const GRADIENT: &[u8] = b" .:!/r(l1Z4H9W8$@";
 
 #[derive(Debug)]
 pub struct Imaget {
-    img: Option<DynamicImage>
+    img: Option<DynamicImage>,
+    size: Option<(u32, u32)>
 }
 
 impl Imaget {
@@ -35,26 +34,65 @@ impl Imaget {
             move || image::open(path).map_err(|err| eyre::eyre!("Error: {}", err)))
             .await?
             .map_err(|err| eyre::eyre!("Error: {}", err))?;
-        Ok(Self { img: Some(img) })
+        Ok(Self { img: Some(img), size: None })
     }
 
     pub async fn resize(&mut self, nwidth: u32) -> Result<Self> {
         if let Some( img ) = &self.img {
             let img = img.clone();
+            let (width, height) = img.dimensions();
+            let nheight = (nwidth as f32 * height as f32 / width as f32 * 0.55) as u32;
             let resized_img = task::spawn_blocking(move || {
-                let (width, height) = img.dimensions();
-                let nheight = (nwidth as f32 * height as f32 / width as f32 * 0.55) as u32;
                 img.resize_exact(nwidth, nheight, image::imageops::FilterType::Nearest)
             })
             .await?;
 
-            Ok(Self { img: Some(resized_img)})
+            Ok(Self { img: Some(resized_img), size: Some((nwidth, nheight)) })
         } else {
             Err(eyre::eyre!("File does not exist!\nNothing to resize."))
         }
     }
 
-    pub async fn save(&self) -> Result<()> {
-        todo!();
+    pub async fn save(&self, path: impl Into<PathBuf>) -> Result<()> {
+        if let Some( img ) = &self.img {
+            let img = img.clone();
+            let path = path.into();
+            task::spawn_blocking(move || { img.save(path) }).await?.unwrap();
+            Ok(())
+        } else {
+            Err(eyre::eyre!("File does not exist!\nNothing to save."))
+        }
+    }
+
+    pub async fn img_to_ascii_convertor(&self) -> Result<String, eyre::Error> {
+        if let Some(img) = &self.img {
+            let img = img.clone();
+            let size = self.size.ok_or_else(|| eyre::eyre!("Image size not specified"))?;
+            let ascii_art = task::spawn_blocking(move || -> Result<String, eyre::Error> {
+                let ascii_str: String = img
+                    .pixels()
+                    .map(|p| {
+                        let brightness = 0.2126 * p.2[0] as f32
+                                            + 0.7152 * p.2[1] as f32
+                                            + 0.0722 * p.2[2] as f32;
+
+                        let ascii_index = (brightness * (GRADIENT.len() - 1) as f32 / 255.0) as usize;
+                        GRADIENT[ascii_index] as char
+                    })
+                    .collect();
+
+                let mut ascii_lines = Vec::new();
+                for chunk in ascii_str.chars().collect::<Vec<char>>().chunks(size.0 as usize) {
+                    ascii_lines.push(chunk.iter().collect::<String>());
+                }
+
+                Ok(ascii_lines.join("\n"))
+            })
+            .await??;
+
+            Ok(ascii_art)
+        } else {
+            Err(eyre::eyre!("File does not exist!\nNothing to convert."))
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,48 +8,13 @@ use imaget::Imaget;
 
 use std::path::Path;
 
-use image::{
-    DynamicImage,
-    GenericImageView,
-    ImageBuffer,
-    Luma
-};
-
-use tokio::*;
-
-
-
-// #[derive(Debug)]
-// struct Image;
-
-// impl Image {
-//     fn to_grayscale(image: DynamicImage) -> ImageBuffer<Luma<u8>, Vec<u8>> {
-//         image.into_luma8()
-//     }
-
-//     fn map_pixels_to_ascii(image: ImageBuffer<Luma<u8>, Vec<u8>>, width: u32) -> String {
-//         let ascii_str: String = image
-//             .pixels()
-//             .map(|p| {
-//                 let ascii_index = p[0] as usize * (GRADIENT.len() - 1) / 255;
-//                 GRADIENT[ascii_index] as char
-//             })
-//             .collect();
-
-//         let mut ascii_lines = Vec::new();
-//         for chunk in ascii_str.chars().collect::<Vec<char>>().chunks(width as usize) {
-//             ascii_lines.push(chunk.iter().collect::<String>());
-//         }
-
-//         ascii_lines.join("\n")
-//     }
-// }
 
 #[tokio::main]
 async  fn main() {
-    let path = Path::new("./anime.jpg");
-    let width = 150;
+    let path = Path::new("./anime_2.jpg");
+    let width = 100;
 
     let mut image = Imaget::open(path).await.unwrap();
-    image.resize(width).await.unwrap();
+    let ascii_art = image.resize(width).await.expect("Cant resize!").img_to_ascii_convertor().await.unwrap();
+    println!("{}", ascii_art)
 }
